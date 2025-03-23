e0f856f82f5d63c4a7558c6e01d087c3d941fdeb
manas-dhyani <manasdhyani17@gmail.com>
2025-03-23 09:50:00 +0530

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer-nlp"
+AUTHOR_USER_NAME = "manas-dhyani"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "manasdhyani24@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A text summarizer using NLP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},   # tell distutils packages are under src
+    packages=setuptools.find_packages(where="src"),# include all packages under src
+)
