5e2ddd1618dbf2916da0750494488ffca5b324ce
manas-dhyani <manasdhyani17@gmail.com>
2025-07-13 20:17:18 +0530

    refactor: converted notebook experiment into DataIngestion and DataValidation components
    - updated config.yaml and params.yaml
    - created DataIngestion and DataValidation entity classes
    - added logic to ConfigurationManager and respective components
    - updated pipeline, main.py, and app.py for integration

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -1,4 +1,3 @@
-#python function frequently in the code, we use utils to store them we use util to import them
 import os
 from box.exceptions import BoxValueError
 import yaml
@@ -9,50 +8,59 @@ from pathlib import Path
 from typing import Any
 
 
+
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """
-    Read the yaml file and return the ConfigBox object
+    """reads yaml file and returns
 
     Args:
-    path_to_yaml (Path): Path to the yaml file as input
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
 
     Returns:
-    ConfigBox: ConfigBox type object
+        ConfigBox: ConfigBox type
     """
     try:
-        with open(path_to_yaml) as file:
+        with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file read successfully from {path_to_yaml}")
-            return ConfigBox(config)
-    except BoxValueError as e:
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
     
 
+
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
-    """create list of directories 
+    """create list of directories
 
     Args:
-         path_to_directories (list): list of directories
-         ignore_log(bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"Directory created at {path}")
+            logger.info(f"created directory at: {path}")
+
+
 
 @ensure_annotations
 def get_size(path: Path) -> str:
-    """get size of the directory
+    """get size in KB
 
     Args:
-        path (Path): path to the directory
+        path (Path): path of the file
 
     Returns:
-        str: size in kb
+        str: size in KB
     """
-    size_in_kb = round(os.path.getsize(path) / 1024)
+    size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
+
+    
